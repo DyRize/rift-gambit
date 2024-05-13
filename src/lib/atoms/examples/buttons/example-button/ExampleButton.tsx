f895b21af9d8739469e2e2451c3cec7f0ab342a8
Dylan Le Flour <contact@dylanleflour.fr>
2024-05-13 23:53:57 +0200

    feat(shadcn): init shadcn/ui configuration

--- src/lib/atoms/examples/buttons/example-button/ExampleButton.tsx
+++ src/lib/atoms/examples/buttons/example-button/ExampleButton.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Button } from '@/src/components/ui/button';
 
 type ExampleButtonProps = {} & React.ComponentPropsWithRef<'button'>;
 
 const ExampleButton = React.forwardRef<HTMLButtonElement, ExampleButtonProps>(
   function ExampleButton({ children, ...rest }, ref) {
     return (
-      <button ref={ref} {...rest}>
+      <Button ref={ref} {...rest}>
         {children}
-      </button>
+      </Button>
     );
   },
 );
